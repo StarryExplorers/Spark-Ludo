78c88024fdba7ff6b3ffe07f98f2c52ea1fb4132
StarryExplorers <starryexplorers@gmail.com>
2025-06-21 18:35:51 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const cacheName = 'spark-ludo-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
